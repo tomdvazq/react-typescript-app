0de9c501f716ed9ffc286fd31c2e8df2a6ba446e
timi <tomasvazquezdev@gmail.com>
2023-08-23 16:35:58 -0300

    create counter

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const Counter = () => {
+    const [counter, setCounter] = useState(0);
+
+    const incrementar = ( numero: number = 1 ):void => {
+        setCounter(counter + numero)
+    }
+
     return (
         <div className="mt-5">
             <h3>Counter</h3>
+            <span>Valor: {counter}</span>
+            <button
+            onClick={ () => incrementar()} 
+            className="btn btn-outline-primary mt-2">
+                +1
+            </button>
+            <button 
+            onClick={ () => incrementar(2)}
+            className="btn btn-outline-primary mt-2">
+                +2
+            </button>
         </div>
     )
 }
